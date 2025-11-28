685734d85656816737748163f5c44fdfb9a51a70
AthanasiaPet <athanasia.petsa@yahoo.com>
2025-11-28 23:53:22 +0200

    feat: add CounterAdvanced component

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ function App() {
                 {/*<ClassComponentWithState />*/}
                 {/*<Counter />*/}
                 {/*<NameChanger />*/}
+                {/*<CounterWithMoreStates />*/}
                 <CounterWithMoreStates />
             </LayOut>
 
